95ce49b6ecbf7383355d31e5e67e3a6dd8dd16b1
GermanFredes <166564862+GermanFredes@users.noreply.github.com>
2026-08-14 21:36:22 -0300

    Actualizar App.jsx

--- Main/src/App.jsx
+++ Main/src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import elementos from "./data/elementos";
 import Tabla from "./components/Tabla";
 import Detalle from "./components/Detalle";
-import "./App.css"
-
+import "./App.css";
 
 export default function App() {
-  const [seleccionado, setSeleccionado] = useState(elementos[0]);
+  const [seleccionado, setSeleccionado] = 
+  useState(null);
+  function cerrar() {
+    setSeleccionado(null)
+	
+}
 
   return (
     <>
-      <h1> Tabla Periódica</h1>
+      <h1>Tabla Periódica</h1>
 
       <Tabla
         elementos={elementos}
         seleccionar={setSeleccionado}
       />
 
-      <Detalle elemento={seleccionado} />
+      {seleccionado
+      && 
+      <Detalle
+        elemento={seleccionado}
+        cerrar={cerrar}/>}
     </>
   );
 }
