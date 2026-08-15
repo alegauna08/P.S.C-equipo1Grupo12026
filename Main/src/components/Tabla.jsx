aaf79c3d9795209709bc03965ba9fcbb4f248084
GermanFredes <166564862+GermanFredes@users.noreply.github.com>
2026-08-14 21:42:57 -0300

    Actualizar Tabla.jsx

--- Main/src/components/Tabla.jsx
+++ Main/src/components/Tabla.jsx
@@ -4,10 +4,9 @@ import "./Tabla.css";
 export default function Tabla({ elementos, seleccionar }) {
   return (
   <>
-  {/* inclan */}
-<div className="leyenda">
+ <div className="leyenda">
   <div><span className="color metal-alcalino"></span> Metal alcalino</div>
-  <div><span className="color metal-alcalinotérreo"></span> Metal alcalinotérreo</div>
+  <div><span className="color metal-alcalinoterreo"></span> Metal alcalinotérreo</div>
   <div><span className="color metal-transicion"></span> Metal de transición</div>
   <div><span className="color metal-post-transicion"></span> Metal post-transición</div>
   <div><span className="color metaloide"></span> Metaloide</div>
